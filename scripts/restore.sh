e4c76f3f2b87af4cdb458131367dc1c1303893db
Airscript <francesco@airscript.it>
2023-09-03 03:08:49 +0200

    feat(scripts): add restore

---
+++ scripts/restore.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "[1/4] Creating configs backup variable..."
+export ANVIM=~/.config/nvim.bak
+
+echo "[2/4] Checking if backup is present..."
+if [[ -d "$ANVIM" ]] 
+  then 
+    echo "[3/4] Restoring backup..."
+    mv ~/.config/nvim.bak ~/.config/nvim
+
+  else
+    echo "[3/4] Backup not found, restore skipped."
+fi
+
+echo "[4/4] Restore process completed successfully!"
