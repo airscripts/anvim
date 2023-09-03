468238c4e2ed76e7c045a00bbee84a56fd456179
Airscript <francesco@airscript.it>
2023-09-03 03:02:08 +0200

    feat(scripts): add backup

---
+++ scripts/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "[1/4] Creating configs variable..."
+export ANVIM=~/.config/nvim
+
+echo "[2/4] Checking if configs are present..."
+if [[ -d "$ANVIM" ]] 
+  then 
+    echo "[3/4] Making configs backup inside ~/.config/nvim.bak..."
+    mv ~/.config/nvim ~/.config/nvim.bak
+
+  else
+    echo "[3/4] Configs not found, backup skipped."
+fi
+
+echo "[4/4] Backup procedure completed successfully!"
