f7c489d2d4ab2c1f43ce306de641a072531955aa
Airscript <francesco@airscript.it>
2023-09-03 03:02:02 +0200

    feat(scripts): add install

---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "[1/4] Creating configs variable..."
+export ANVIM=~/.config/nvim
+
+echo "[2/4] Creating nvim configs folder..."
+mkdir -p $ANVIM
+
+echo "[3/4] Copying anvim configs..."
+cp -r configs/* $ANVIM
+echo "[4/4] Installation successful!"
