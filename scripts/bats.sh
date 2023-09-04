ab4def459902e2e0e3569ca8cb4db4d2d644422a
Airscript <francesco@airscript.it>
2023-09-04 19:42:40 +0200

    test: add bats framework

---
+++ scripts/bats.sh
@@ -0,0 +1,3 @@
+git submodule add https://github.com/bats-core/bats-core.git tests/bats
+git submodule add https://github.com/bats-core/bats-support.git tests/helpers/bats-support
+git submodule add https://github.com/bats-core/bats-assert.git tests/helpers/bats-assert
